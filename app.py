c334b7719470cdefa7e1f3f04f1ae8098fd3ff65
Aitect <lean-architect@proton.me>
2025-09-18 05:39:44 +0000

    fix: port

--- app.py
+++ app.py
@@ -107,4 +107,4 @@ def post_handler():
         return Response("ok", mimetype="text/plain", status=500)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8010)
